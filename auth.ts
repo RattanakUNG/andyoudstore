145eeca983979bfc912be62368362955e4bb7da5
RattanakUNG <ungrattanak@gmail.com>
2025-12-20 12:55:44 +0700

    add jwt authentication

--- auth.ts
+++ auth.ts
@@ -71,12 +71,63 @@ export const config = {
       session.user.role = token.role;
       session.user.name = token.name;
 
+      // console.log(token);
+
       // If there is an update, set the user name
       if (trigger === "update" && user?.name) {
         session.user.name = user.name;
       }
 
       return session;
+    },
+    async jwt({ token, user, trigger, session }) {
+      // Assign user fields to token
+      if (user) {
+        token.id = user.id;
+        token.role = user.role;
+
+        // If user has no name then use the email
+        if (user.name === "NO_NAME") {
+          token.name = user.email!.split("@")[0];
+
+          // Update database to reflect the token name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+
+        // if (trigger === 'signIn' || trigger === 'signUp') {
+        //   const cookiesObject = await cookies();
+        //   const sessionCartId = cookiesObject.get('sessionCartId')?.value;
+
+        //   // if (sessionCartId) {
+        //   //   const sessionCart = await prisma.cart.findFirst({
+        //   //     where: { sessionCartId },
+        //   //   });
+
+        //   //   if (sessionCart) {
+        //   //     // Delete current user cart
+        //   //     await prisma.cart.deleteMany({
+        //   //       where: { userId: user.id },
+        //   //     });
+
+        //   //     // Assign new cart
+        //   //     await prisma.cart.update({
+        //   //       where: { id: sessionCart.id },
+        //   //       data: { userId: user.id },
+        //   //     });
+        //   //   }
+        //   // }
+        // }
+      }
+
+      // Handle session updates
+      if (session?.user.name && trigger === "update") {
+        token.name = session.user.name;
+      }
+
+      return token;
     },
   },
 } satisfies NextAuthConfig;
